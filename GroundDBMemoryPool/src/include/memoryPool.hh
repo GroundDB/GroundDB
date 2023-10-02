6bd17e6fa79616e51fe037396990248086de81ae
Shicheng Fang <fangshicheng15@gmail.com>
2023-10-02 18:53:56 +0000

    memory pool api def

--- GroundDBMemoryPool/src/include/memoryPool.hh
+++ GroundDBMemoryPool/src/include/memoryPool.hh
@@ -1,2 +1,21 @@
 #pragma once
+#include <stddef.h>
+
+typedef struct remote_memory_pool_client
+{
+    // TBD
+};
+
+struct remote_memory_pool_client connect(
+    char *server_ip,
+    size_t size,
+    int port,
+    char *local_buffer,
+    size_t local_buffer_size);
+
+void allocate_memory_region(
+    size_t size, /* number of bytes to allocate*/
+    struct remote_memory_pool_client *rmpc);
+void read(size_t mem_reg_id, size_t remote_offset, size_t size, char *local_buffer, struct remote_memory_pool_client *);
+void write(size_t mem_reg_id, size_t remote_offset, size_t size, char *local_buffer, struct remote_memory_pool_client *);
 #include "add.hh"
