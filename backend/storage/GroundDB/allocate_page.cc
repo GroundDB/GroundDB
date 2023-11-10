8cc3209c029fd03aca314d476d566fe0d96e1fd3
te-guo <teguo@outlook.com>
2023-11-10 14:23:30 +0000

    introduce page id array

--- backend/storage/GroundDB/allocate_page.cc
+++ backend/storage/GroundDB/allocate_page.cc
@@ -5,7 +5,7 @@ namespace mempool
 {
 
 int allocate_page(struct memory_region *&memreg, struct resources *res, const char* src, size_t page_cnt, size_t page_size){
-	return register_mr(memreg, res, src, page_cnt * page_size);
+	return register_mr(memreg, res, src, page_cnt * (page_size + sizeof(KeyType)));
 }
 
 } // namespace mempool
