37411076db93ef6b21230813231fc966ecfd58a5
te-guo <teguo@outlook.com>
2024-01-03 01:55:38 +0000

    remove obsolete code

--- GroundDBMemoryPool/src/main_client.cc
+++
@@ -1,9 +0,0 @@
-#include <memoryPool.hh>
-int main()
-{
-    mempool::connectServer(
-        "127.0.0.1",
-        122189,
-        NULL,
-        1);
-}
--- GroundDBMemoryPool/src/test/add.test.cc
+++
@@ -1,7 +0,0 @@
-#include <gtest/gtest.h>
-#include <memoryPool.hh>
-// Demonstrate some basic assertions.
-TEST(AddTest, BasicAssertions)
-{
-    EXPECT_EQ(7, add(2, 5));
-}
